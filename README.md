ab5985a6bf5db574616f9f101e931045057c30a7
Stulz Nicolas <StulzN@ad.net.fr.ch>
2019-02-14 16:24:59 +0100

    Dev: main

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # photo_utils
 Tools to handle pictures (resizing, ...)
+
+## Installation
+
+A virtualdev with Python 3 (virtualenv virtualenv) is required
+
+Clone the project
+
+Run python resizer.py -i <input_img> -s <size> -o <output_img>
+
+Run python resizer.py -h for help
+
+Please see require.txt for requirements (pip install -r require.txt).
+
+## Test
+
+Can resize a TIFF to JPEG or PNG(Other format are actually not tested)
--- resizer.py
+++ resizer.py
@@ -5,7 +5,7 @@
 # Author: Nicolas Stulz
 ###
 
-# python resizer.py -i img/JATH_52577.tif -s 1100 -o img/ye4.jpg -d
+# example python resizer.py -i img/JATH_52577.tif -s 1100 -o img/xxx.jpg
 
 import os, sys
 from pathlib import Path
@@ -35,90 +35,95 @@ def convert_size_to_k(size):
         size //= 1000
         return "_".__add__(str(size)).__add__("k")
 
-parser = argparse.ArgumentParser(description='This program allows you to resize a picture')
-optional = parser._action_groups.pop()
-required = parser.add_argument_group('required arguments')
+def main():
 
-required.add_argument('-i','--input', type=str,
-    help='The input file name (and path if not current folder)', required=True)
-required.add_argument('-s','--size', type=int,
-    help='The size of the longest side of the image', required=True)
-required.add_argument('-o','--output', type=str,
-    help='The output file name (and path if not current folder)', required=True)
+    parser = argparse.ArgumentParser(description='This program allows you to resize a picture')
+    optional = parser._action_groups.pop()
+    required = parser.add_argument_group('required arguments')
 
-optional.add_argument('-v','--verbosity', type=int,
-    help='Set to 1 to print, set 0 to not print anything', default=1)
-optional.add_argument('-f','--format', type=str,
-    help='Set the file format [jpg|png] default is jpg', default=DEFAULT_FORMAT)
-optional.add_argument('-d','--dimension', action="store_true", default=False,
-    help='Just add -d to automatically add the longest side dimension to the resized image. Example for a x.jpg (2000x1500) -> x_2k.jpg')
-optional.add_argument('-c','--compare', action="store_true", default=False,
-    help='Just add -c to compare the input and output file')
+    required.add_argument('-i','--input', type=str,
+        help='The input file name (and path if not current folder)', required=True)
+    required.add_argument('-s','--size', type=int,
+        help='The size of the longest side of the image', required=True)
+    required.add_argument('-o','--output', type=str,
+        help='The output file name (and path if not current folder)', required=True)
 
-parser._action_groups.append(optional)
+    optional.add_argument('-v','--verbosity', type=int,
+        help='Set to 1 to print, set 0 to not print anything', default=1)
+    optional.add_argument('-f','--format', type=str,
+        help='Set the file format [jpg|png] default is jpg', default=DEFAULT_FORMAT)
+    optional.add_argument('-d','--dimension', action="store_true", default=False,
+        help='Just add -d to automatically add the longest side dimension to the resized image. Example for a x.jpg (2000x1500) -> x_2k.jpg')
+    optional.add_argument('-c','--compare', action="store_true", default=False,
+        help='Just add -c to compare the input and output file')
 
-args = parser.parse_args()
+    parser._action_groups.append(optional)
 
-original_photo_name = args.input
-max_size = args.size
-output_name = args.output
-verb = args.verbosity
+    args = parser.parse_args()
 
-img_format = args.format
+    original_photo_name = args.input
+    max_size = args.size
+    output_name = args.output
+    verb = args.verbosity
 
-size_info = ""
+    img_format = args.format
 
-if args.dimension:
-    size_info = convert_size_to_k(max_size)
+    size_info = ""
 
-new_filename = os.path.splitext(output_name)[0]
+    if args.dimension:
+        size_info = convert_size_to_k(max_size)
 
-try:
+    new_filename = os.path.splitext(output_name)[0]
 
-    img = Image.open(original_photo_name, mode='r')
+    try:
 
-    dpi = img.info["dpi"]
+        img = Image.open(original_photo_name, mode='r')
 
-    img.thumbnail((max_size, max_size), Image.ANTIALIAS)
+        dpi = img.info["dpi"]
 
-    resized_photo_name = new_filename.__add__(size_info).__add__(FORMATS[img_format]["ext"])
+        img.thumbnail((max_size, max_size), Image.ANTIALIAS)
 
-    if original_photo_name == resized_photo_name:
-        print("Input and output files are same...")
-        print("Cannot resize...")
-        sys.exit(0)
-    else:
-        img.save(resized_photo_name, FORMATS[img_format]["mime"], quality=100, dpi=dpi)
+        resized_photo_name = new_filename.__add__(size_info).__add__(FORMATS[img_format]["ext"])
 
-    img.close()
+        if original_photo_name == resized_photo_name:
+            print("Input and output files are same...")
+            print("Cannot resize...")
+            sys.exit(0)
+        else:
+            img.save(resized_photo_name, FORMATS[img_format]["mime"], quality=100, dpi=dpi)
 
-    if verb == 1 and args.compare is False:
+        img.close()
 
-        print("\n")
-        decor_print(60, 57 * "#")
-        decor_print(60, "")
-        decor_print(60, "Photo " + resized_photo_name + " has been saved...")
-        decor_print(60, "")
-        decor_print(60, 57 * "#")
-        print("\n")
+        if verb == 1 and args.compare is False:
 
-    if args.compare:    #TODO Center the 2 columns
+            print("\n")
+            decor_print(60, 57 * "#")
+            decor_print(60, "")
+            decor_print(60, "Photo " + resized_photo_name + " has been saved...")
+            decor_print(60, "")
+            decor_print(60, 57 * "#")
+            print("\n")
 
-        in_img = Image.open(original_photo_name, mode='r')
-        out_img = Image.open(resized_photo_name, mode='r')
+        if args.compare:    #TODO Center the 2 columns
 
-        print("\n" + "#" * 60)
-        decor_print(60, "Compare")
-        decor_print(60, "")
-        print("#    Name: " + in_img.filename + " |    " + out_img.filename)
-        decor_print(60, "")
-        decor_print(60, "")
-        print("#    size:        " + str(in_img.size)+ " |    " + str(out_img.size))
-        print("#    dpi:         " + str(in_img.info["dpi"])+ "    | " + str(out_img.info["dpi"]))
-        print("#    mode:        " + str(in_img.mode)+ " |    " + str(out_img.mode))
-        print("#    format:      " + str(in_img.format)+ " |    " + str(out_img.format))
-        decor_print(60, "")
-        print("#" * 60 + "\n")
+            in_img = Image.open(original_photo_name, mode='r')
+            out_img = Image.open(resized_photo_name, mode='r')
 
-except IOError:
-    print("Cannot resize for '%s'" % original_photo_name)
+            print("\n" + "#" * 60)
+            decor_print(60, "Compare")
+            decor_print(60, "")
+            print("#    Name: " + in_img.filename + " |    " + out_img.filename)
+            decor_print(60, "")
+            decor_print(60, "")
+            print("#    size:        " + str(in_img.size)+ " |    " + str(out_img.size))
+            print("#    dpi:         " + str(in_img.info["dpi"])+ "    | " + str(out_img.info["dpi"]))
+            print("#    mode:        " + str(in_img.mode)+ " |    " + str(out_img.mode))
+            print("#    format:      " + str(in_img.format)+ " |    " + str(out_img.format))
+            decor_print(60, "")
+            print("#" * 60 + "\n")
+
+    except IOError:
+        print("Cannot resize for '%s'" % original_photo_name)
+
+if __name__ == "__main__":
+    main()
